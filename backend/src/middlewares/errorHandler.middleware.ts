031a169fdbac88b773388675d62549f91600ca13
Nehil-nama <namanehil2004@gmail.com>
2025-03-21 16:01:30 +0530

    custom app error managed

--- backend/src/middlewares/errorHandler.middleware.ts
+++ backend/src/middlewares/errorHandler.middleware.ts
@@ -1,5 +1,6 @@
 import { ErrorRequestHandler } from "express";
 import { HTTPSTATUS } from "../config/http.config";
+import { AppError } from "../utils/appError";
 
 export const errorHandler: ErrorRequestHandler = (error, req, res, next): any => {
     console.error(`Error Occurred on PATH: ${req.path}`, error);
@@ -9,6 +10,14 @@ export const errorHandler: ErrorRequestHandler = (error, req, res, next): any =>
             message: "Invalid JSON format. please check your request and try again",
         });
     }
+
+    if(error instanceof AppError){
+        return res.status(error.statusCode).json({
+            message: error.message,
+            errorCode: error.errorCode,
+        });
+    }
+
     return res.status(HTTPSTATUS.INTERNAL_SERVER_ERROR).json({
         message: "Internal server error",
         error: error?.message || "Unknown error occurred",
