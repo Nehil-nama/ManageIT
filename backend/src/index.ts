1b2349824e43d430732c909a20a17a670382de43
Nehil-nama <namanehil2004@gmail.com>
2025-03-31 21:10:08 +0530

    Completed Task Endpoints.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,12 +13,12 @@ import { ErrorCodeEnum } from "./enums/error-code.enum";
 import "./config/passport.config";
 import passport from "passport";
 import authRoutes from "./routes/auth.route";
-import uesrRoutes from "./routes/user.route";
 import userRoutes from "./routes/user.route";
 import isAuthenticated from "./middlewares/isAuthenticate.middleware";
 import workspaceRoutes from "./routes/workspace.route";
 import memberRoutes from "./routes/member.route";
 import projectRoutes from "./routes/project.route";
+import taskRoutes from "./routes/task.route";
 const app = express();
 const BASE_PATH = config.BASE_PATH;
 
@@ -62,6 +62,7 @@ app.use(`${BASE_PATH}/user`, isAuthenticated ,userRoutes);
 app.use(`${BASE_PATH}/workspace`, isAuthenticated, workspaceRoutes);
 app.use(`${BASE_PATH}/member`, isAuthenticated, memberRoutes);
 app.use(`${BASE_PATH}/project`, isAuthenticated, projectRoutes);
+app.use(`${BASE_PATH}/task`, isAuthenticated, taskRoutes);
 
 
 app.use(errorHandler);
